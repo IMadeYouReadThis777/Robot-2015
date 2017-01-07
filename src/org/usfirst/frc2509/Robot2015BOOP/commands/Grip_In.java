48fa5a75da8d2330b302028e5114bdde2f1b6c83
IMadeYouReadThis777 <calebfox0@student.isd423.org>
2017-01-07 15:38:58 -0600

    Fixed Lift.java, need to fix line 93 bracket error

--- src/org/usfirst/frc2509/Robot2015BOOP/commands/Grip_In.java
+++ src/org/usfirst/frc2509/Robot2015BOOP/commands/Grip_In.java
@@ -1,19 +1,22 @@
 package org.usfirst.frc2509.Robot2015BOOP.commands;
 
+import org.usfirst.frc2509.Robot2015BOOP.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class Grip_In extends Command {
-
     public Grip_In() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.grips);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.grips.In();
     }
 
     // Called repeatedly when this Command is scheduled to run
--- src/org/usfirst/frc2509/Robot2015BOOP/subsystems/Grips.java
+++ src/org/usfirst/frc2509/Robot2015BOOP/subsystems/Grips.java
@@ -1,18 +1,43 @@
 package org.usfirst.frc2509.Robot2015BOOP.subsystems;
 
+import org.usfirst.frc2509.Robot2015BOOP.RobotMap;
+import org.usfirst.frc2509.Robot2015BOOP.commands.Grip_Out;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Grips extends Subsystem {
-    
+    private final DoubleSolenoid piston = RobotMap.gripsGripDBL_Solenoid;
+    private boolean Position = false;
+    public void initDefaultCommand() {
+    	setDefaultCommand(new Grip_Out());
+    }
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-}
+    public Object grip;
 
+	public void In() {
+		
+		piston.set(DoubleSolenoid.Value.kForward);
+		Position = true;
+		// TODO Auto-generated method stub
+		
+	}
+	public void Out() {
+		piston.set(DoubleSolenoid.Value.kReverse);
+		Position = false;
+	}
+	public void Move(){
+		if(Position == false){
+			In();
+		}else if(Position==true){
+			Out();
+		}else{
+				piston.set(DoubleSolenoid.Value.kOff);
+		}
+	}
+}
